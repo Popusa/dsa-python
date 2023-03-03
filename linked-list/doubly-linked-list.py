47baad1695f7f10c013db1f62cb99ef9d3f34e50
Popusa <danielmaged.3@gmail.com>
2023-03-03 17:48:13 +0200

    finished doubly linked list code

--- linked-list/doubly-linked-list.py
+++ linked-list/doubly-linked-list.py
@@ -11,135 +11,128 @@ class doubly_linked_list:
     def __init__(self):
         self.head = None
     
-    def insert_at_beginning(self,data):
-        added_node = node(data,self.head)
-        self.head = added_node
-    
-    def insert_at_end(self,data):
+    def print_forward(self):
         if self.head == None:
-            self.head = node(data,None)
+            print("linked list is empty.")
             return
-        else:
-            itr = self.head
-            while itr.next:
-                itr = itr.next
-            itr.next = node(data,None)
-    # Setting new_list flag to true will add list of values to current list. If it is kept false,
-    # a new linked list will be made with the list of values only.
-    def insert_values(self,arr,new_list = False):
-        if  not new_list:
-            self.head = None
-        else:
-            pass
-        for data in arr:
-            self.insert_at_end(data)
+        
+        itr = self.head
+        ll_str = ""
+        while itr:
+            if itr.next == None:
+                ll_str = ll_str + itr.data
+            else:    
+                ll_str = ll_str + itr.data + " ---> "
+            itr = itr.next
 
-    def insert_after_value(self, data_after, data_to_insert):
+        print(ll_str)
+
+    def get_last_node(self):
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        return itr
+
+    def print_backward(self):
+        last_node = None
         if self.head == None:
-            print("Linked list is empty.")
+            print("linked list is empty.")
             return
         
-        if self.head.data == data_after:
-            self.head.next = node(data_to_insert,self.head.next)
-        
-        found_data = False
+        itr = self.get_last_node()
+        ll_str = ""
+        while itr:
+            if itr.prev == None:
+                ll_str = ll_str + itr.data
+            else:    
+                ll_str = ll_str + itr.data + " ---> "         
+            itr = itr.prev
+        print(ll_str)
+
+    def get_last_node(self):
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        return itr
+
+    def get_length(self):
+        count = 0
         itr = self.head
         while itr:
-            if itr.data == data_after:
-                itr.next = node(data_to_insert,itr.next)
-                found_data = True
-                break
-            else:
-                itr = itr.next
-        if not found_data:
-            print("Data not found.")
+            count+=1
+            itr = itr.next
 
-    #Setting the first_only flag to be false will cause the list to find all nodes with the corresponding data and remove them.
-    #Keeping it at true will only remove the first occurrence.
-    def remove_by_value(self, data,first_only = True):
+        return count
+
+    def insert_at_begining(self, data):
+        if self.head == None:
+            node = node(data, self.head, None)
+            self.head = node
+        else:
+            node = node(data, self.head, None)
+            self.head.prev = node
+            self.head = node
+
+    def insert_at_end(self, data):
         if self.head is None:
-            print("Linked list is empty.")
+            self.head = node(data, None, None)
             return
 
-        if self.head.data == data:
-            self.head = self.head.next
+        itr = self.head
+
+        while itr.next:
+            itr = itr.next
+
+        itr.next = node(data, None, itr)
+
+    def insert_at(self, index, data):
+        if index<0 or index>self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.insert_at_begining(data)
             return
 
-        counter = 0
-        found_data = False
+        count = 0
         itr = self.head
         while itr:
-            if itr.data == data:
-                found_data = True
-                self.remove_at_index(counter)
-                if first_only:
-                    break
-                else:
-                    itr.next = next
-            else:
-                itr = itr.next
-                counter = counter + 1
-        
-        if not found_data:
-            print("Data not found.")
-
-    def remove_beginning(self):
-        self.head = self.head.next
-    
-    def remove_end(self):
-        itr = self.head
-        counter = 0
-        while itr:
-            if counter == self.get_length() - 2:
-                itr.next = None
-            else:
-                itr = itr.next
-                counter = counter + 1
-
-    def insert_at_index(self,index,data):
-        if index < 0 or index >= self.get_length():
-            print("Invalid Index")
-            return
-        
-        if index == 0:
-            self.insert_at_beginning(data)
-            return
-
-        if index == self.get_length() - 1:
-            self.insert_at_end(data)
-        
-        counter = 0
-        itr = self.head
-        while itr:
-            if counter == index - 1:
-                itr.next = node(data,itr.next)
+            if count == index - 1:
+                node = node(data, itr.next, itr)
+                if node.next:
+                    node.next.prev = node
+                itr.next = node
                 break
-            else:
-                itr = itr.next
-                counter = counter + 1
 
-    def remove_at_index(self,index):
-        if index < 0 or index >= self.get_length():
-            print("Invalid Index")
+            itr = itr.next
+            count += 1
+
+    def remove_at(self, index):
+        if index<0 or index>=self.get_length():
+            raise Exception("Invalid Index")
+
+        if index==0:
+            self.head = self.head.next
+            self.head.prev = None
             return
 
-        if index == 0:
-            self.remove_beginning()
-            return
-        
-        if index == self.get_length() - 1:
-            self.remove_end()
-            return
-        else:
-            counter = 0
-            itr = self.head
-            while itr:
-                if counter == index - 1:
-                    itr.next = itr.next.next
-                    break
-                else:
-                    counter = counter + 1
-                    itr = itr.next
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index:
+                itr.prev.next = itr.next
+                if itr.next:
+                    itr.next.prev = itr.prev
+                break
+
+            itr = itr.next
+            count+=1
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
 
     def print_ll(self):
         if self.head == None:
@@ -166,15 +159,5 @@ class doubly_linked_list:
 if __name__ == '__main__':
     ll = doubly_linked_list()
     ll.insert_values(["banana","mango","grapes","orange"])
-    ll.print_ll()
-    ll.insert_after_value("mango","apple")
-    ll.print_ll()
-    ll.remove_by_value("orange")
-    ll.print_ll()
-    ll.remove_by_value("figs")
-    ll.print_ll()
-    ll.remove_by_value("banana")
-    ll.remove_by_value("mango")
-    ll.remove_by_value("apple")
-    ll.remove_by_value("grapes")
-    ll.print_ll()
+    ll.print_forward()
+    ll.print_backward()
